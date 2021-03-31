df30597e360b19f0cd85bb2dd4ae15e4c9d7696f
yeonghyeonSeok <syh4834@naver.com>
2021-03-31 12:29:38 +0900

    Refactor: /anaylze return 메시지 추가

--- routes/analyze.js
+++ routes/analyze.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const upload = require("../config/multer");
+const defaultRes = require("../module/utils/utils");
+const statusCode = require("../module/utils/statusCode");
 
 /*
 대화내용 분석
@@ -9,7 +11,16 @@ URL          : /analyze
 BODY         : text = 대화내용
 */
 router.post("/", upload.single("text"), async (req, res) => {
-  console.log(req.file.location);
+  if (req.file == undefined) {
+    return res
+      .status(200)
+      .send(defaultRes.successFalse(statusCode.OK, "파일전송실패"));
+  }
+  return res
+    .status(200)
+    .send(
+      defaultRes.successTrue(statusCode.OK, "파일전송성공", req.file.location)
+    );
 });
 
 module.exports = router;
